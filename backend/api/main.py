d94ca6aec10c09ac397de556788b8fd6e8e0935e
Ayesha Nasir <153952026+AyeshaNasirWebDeveloper@users.noreply.github.com>
2026-01-11 19:04:21 +0500

    changes for deployment

--- backend/api/main.py
+++ backend/api/main.py
@@ -3,7 +3,6 @@ from src.database import create_db_and_tables
 from dotenv import load_dotenv
 import os
 
-# Load environment variables from .env file
 load_dotenv()
 
 from src.api.v1.auth import router as auth_router
@@ -12,9 +11,6 @@ from src.api.v1.tasks import tasks_router
 app = FastAPI()
 app.include_router(auth_router)
 app.include_router(tasks_router)
-
-# In a real application, you would connect to your database here.
-# For now, we'll just define a placeholder for database connection logic.
 
 @app.on_event("startup")
 def on_startup():
