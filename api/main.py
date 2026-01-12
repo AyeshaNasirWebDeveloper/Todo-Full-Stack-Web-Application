a04ad0bfa7e1ccda13d6afd3b4e63fd0cdb7f716
Ayesha Nasir <153952026+AyeshaNasirWebDeveloper@users.noreply.github.com>
2026-01-13 01:41:39 +0500

    changes for deployment

--- api/main.py
+++ api/main.py
@@ -1,21 +1,6 @@
 from fastapi import FastAPI
-from src.database import create_db_and_tables
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-from src.api.v1.auth import router as auth_router
-from src.api.v1.tasks import tasks_router
 
 app = FastAPI()
-app.include_router(auth_router)
-app.include_router(tasks_router)
-
-@app.on_event("startup")
-def on_startup():
-    print("Application startup")
-    create_db_and_tables()
 
 @app.get("/")
 def read_root():
